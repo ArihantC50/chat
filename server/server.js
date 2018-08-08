4238e0ff22f4b29906e47f8fb146aa5da4adf540
Arihant <arihant@arihant-Satellite-C50-B>
2018-08-08 16:03:39 +0530

    completed

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express  = require('express');
 const socketio = require('socket.io');
+const {generateMessage} = require('./utils/message');
 const publicpath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 // console.log(__dirname + '/../public');
@@ -14,24 +15,22 @@ app.use(express.static(publicpath));
 
 io.on('connection',(socket)=>{
   console.log('new user connected');
-socket.emit('newMessage',{
-  from: 'Admin',
-  text: 'Welcom to the chat',
-  createdAT: new Date().getTime()
-});
-socket.broadcast.emit('newMessage',{
-  from: 'Admin',
-  text: 'New User Join',
-  createdAT: new Date().getTime()
-});
+socket.emit('newMessage', generateMessage('Admin','Welcome to chat app'));
+socket.broadcast.emit('newMessage',generateMessage('Admin','New User Joined'));
+// socket.broadcast.emit('newMessage',{
+//   from: 'Admin',
+//   text: 'New User Join',
+//   createdAT: new Date().getTime()
+// });
 
   socket.on('CreateMessage',(message)=>{
     console.log('CreateMessage',message);
-    io.emit('newMessage',{
-      from:message.from,
-      text:message.text,
-      createAT: new Date().getTime()
-    });
+    io.emit('newMessage',generateMessage(message.from,message.text));
+    // io.emit('newMessage',{
+    //   from:message.from,
+    //   text:message.text,
+    //   createAT: new Date().getTime()
+    // });
     // socket.broadcast.emit('newMessage',{
     //   from:message.from,
     //   text:message.text,
---
+++ server/utils/message.js
@@ -0,0 +1,8 @@
+var generateMessage = (from,text) => {
+  return {
+    from,
+    text,
+    createdAT: new Date().getTime()
+  };
+};
+module.exports = {generateMessage};
